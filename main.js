ad6a4784bb15933127c72231eb3cdfe4c6bee818
blackneko1234 <blackneko1234@hotmail.com>
2021-07-01 22:09:32 +0700

    Num 3

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            description: 'This is boots'
-            
+            description: 'This is boots',
+            image: './assets/images/socks_green.jpg' ,
+            link: 'https://www.camt.cmu.ac.th'
+
         }
     }
 })
