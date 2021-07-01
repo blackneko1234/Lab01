531c70cfbd7cef71a5ff4033698a77f66ccbcde4
blackneko1234 <blackneko1234@hotmail.com>
2021-07-01 22:47:26 +0700

    Num 5

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const app = Vue.createApp({
         return {
             product: 'Shoes',
             description: 'This is boots',
-            image: './assets/images/socks_green.jpg' ,
+            image: './assets/images/socks_green.jpg',
             link: 'https://www.camt.cmu.ac.th',
             inStock: true,
             inventory: 100,
-            onSale:true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                { id: 1234, Text: 'S ' },
+                { id: 1235, Text: 'M ' },
+                { id: 1236, Text: 'L ' },
+            ]
         }
     }
 })
